0d2a4aa46b06f3f433507bcb1da6574fea1da123
uku <hi@uku.moe>
2025-09-15 23:18:17 +0200

    feat: replace mc-heads with spinnable widget
    additionally, reworks the data loading logic for the player info screen, removing widget changes that were not on the render thread

--- src/main/java/com/kevin/tiertagger/tierlist/PlayerInfoScreen.java
+++ src/main/java/com/kevin/tiertagger/tierlist/PlayerInfoScreen.java
@@ -1,56 +1,34 @@
 package com.kevin.tiertagger.tierlist;
 
-import com.kevin.tiertagger.TierCache;
 import com.kevin.tiertagger.TierTagger;
 import com.kevin.tiertagger.model.GameMode;
 import com.kevin.tiertagger.model.PlayerInfo;
-import lombok.Setter;
 import net.minecraft.client.MinecraftClient;
 import net.minecraft.client.gui.DrawContext;
 import net.minecraft.client.gui.screen.Screen;
 import net.minecraft.client.gui.tooltip.Tooltip;
 import net.minecraft.client.gui.widget.ButtonWidget;
+import net.minecraft.client.gui.widget.PlayerSkinWidget;
 import net.minecraft.client.gui.widget.TextWidget;
-import net.minecraft.client.texture.NativeImage;
-import net.minecraft.client.texture.NativeImageBackedTexture;
-import net.minecraft.client.texture.TextureManager;
 import net.minecraft.screen.ScreenTexts;
 import net.minecraft.text.MutableText;
 import net.minecraft.text.Text;
 import net.minecraft.util.Formatting;
-import net.minecraft.util.Identifier;
 import net.uku3lig.ukulib.config.screen.CloseableScreen;
 import org.jetbrains.annotations.NotNull;
 
-import java.io.IOException;
-import java.net.URI;
-import java.net.http.HttpClient;
-import java.net.http.HttpRequest;
-import java.net.http.HttpResponse;
 import java.time.Instant;
 import java.time.ZoneOffset;
 import java.time.format.DateTimeFormatter;
-import java.util.Queue;
-import java.util.concurrent.CompletableFuture;
-import java.util.concurrent.ConcurrentLinkedQueue;
 
-@Setter
 public class PlayerInfoScreen extends CloseableScreen {
-    private static final HttpClient HTTP_CLIENT = HttpClient.newHttpClient();
+    private final PlayerInfo info;
+    private final PlayerSkinWidget skin;
 
-    private String player;
-    private Identifier texture;
-    private PlayerInfo info;
-    private boolean everythingIsAwesome = true;
-
-    /**
-     * Having a queue like this avoids a {@link java.util.ConcurrentModificationException} due to adding drawable children on a different thread
-     */
-    private final Queue<TextWidget> textWidgets = new ConcurrentLinkedQueue<>();
-
-    public PlayerInfoScreen(Screen parent, String player) {
+    public PlayerInfoScreen(Screen parent, PlayerInfo info, PlayerSkinWidget skin) {
         super(Text.of("Player Info"), parent);
-        this.player = player;
+        this.info = info;
+        this.skin = skin;
     }
 
     @Override
@@ -59,36 +37,26 @@ public class PlayerInfoScreen extends CloseableScreen {
                 .dimensions(this.width / 2 - 100, this.height - 27, 200, 20)
                 .build());
 
-        this.fetchTexture(this.player).thenAccept(this::setTexture);
+        this.addDrawableChild(this.skin);
 
-        if (this.info == null) {
-            TierCache.searchPlayer(this.player).thenAccept(this::setInfo)
-                    .whenComplete((v, t) -> {
-                        if (t != null) {
-                            this.everythingIsAwesome = false;
-                        } else {
-                            int rankingHeight = this.info.rankings().size() * 11;
-                            int infoHeight = 56; // 4 lines of text (10 px tall) + 6 px padding
-                            int startY = (this.height - infoHeight - rankingHeight) / 2;
-                            int rankingY = startY + infoHeight;
+        int rankingHeight = this.info.rankings().size() * 11;
+        int infoHeight = 56; // 4 lines of text (10 px tall) + 6 px padding
+        int startY = (this.height - infoHeight - rankingHeight) / 2;
+        int rankingY = startY + infoHeight;
 
-                            for (PlayerInfo.NamedRanking namedRanking : this.info.getSortedTiers()) {
-                                // ugly "fix" to avoid crashes if upstream doesn't have the right names
-                                if (namedRanking.mode() == null) continue;
+        for (PlayerInfo.NamedRanking namedRanking : this.info.getSortedTiers()) {
+            // ugly "fix" to avoid crashes if upstream doesn't have the right names
+            if (namedRanking.mode() == null) continue;
 
-                                TextWidget text = new TextWidget(formatTier(namedRanking.mode(), namedRanking.ranking()), this.textRenderer);
-                                text.setX(this.width / 2 + 5);
-                                text.setY(rankingY);
+            TextWidget text = new TextWidget(formatTier(namedRanking.mode(), namedRanking.ranking()), this.textRenderer);
+            text.setX(this.width / 2 + 5);
+            text.setY(rankingY);
 
-                                String date = DateTimeFormatter.ISO_LOCAL_DATE.withZone(ZoneOffset.UTC).format(Instant.ofEpochSecond(namedRanking.ranking().attained()));
-                                Text tooltipText = Text.literal("Attained: " + date + "\nPoints: " + points(namedRanking.ranking())).formatted(Formatting.GRAY);
-                                text.setTooltip(Tooltip.of(tooltipText));
-
-                                textWidgets.add(text);
-                                rankingY += 11;
-                            }
-                        }
-                    });
+            String date = DateTimeFormatter.ISO_LOCAL_DATE.withZone(ZoneOffset.UTC).format(Instant.ofEpochSecond(namedRanking.ranking().attained()));
+            Text tooltipText = Text.literal("Attained: " + date + "\nPoints: " + points(namedRanking.ranking())).formatted(Formatting.GRAY);
+            text.setTooltip(Tooltip.of(tooltipText));
+            this.addDrawableChild(text);
+            rankingY += 11;
         }
     }
 
@@ -96,57 +64,16 @@ public class PlayerInfoScreen extends CloseableScreen {
     public void render(DrawContext context, int mouseX, int mouseY, float delta) {
         super.render(context, mouseX, mouseY, delta);
 
-        while (!textWidgets.isEmpty()) {
-            this.addDrawableChild(textWidgets.remove());
-        }
+        context.drawCenteredTextWithShadow(this.textRenderer, this.info.name() + "'s profile", this.width / 2, 20, 0xFFFFFFFF);
 
-        String name = this.info == null ? this.player : this.info.name();
-        context.drawCenteredTextWithShadow(this.textRenderer, name + "'s profile", this.width / 2, 20, 0xFFFFFF);
+        int rankingHeight = this.info.rankings().size() * 11;
+        int infoHeight = 56; // 4 lines of text (10 px tall) + 6 px padding
+        int startY = (this.height - infoHeight - rankingHeight) / 2;
 
-        if (this.texture != null && this.info != null) {
-            context.drawTexture(texture, this.width / 2 - 65, (this.height - 144) / 2, 0, 0, 60, 144, 60, 144);
-
-            int rankingHeight = this.info.rankings().size() * 11;
-            int infoHeight = 56; // 4 lines of text (10 px tall) + 6 px padding
-            int startY = (this.height - infoHeight - rankingHeight) / 2;
-
-            context.drawTextWithShadow(this.textRenderer, getRegionText(this.info), this.width / 2 + 5, startY, 0xFFFFFF);
-            context.drawTextWithShadow(this.textRenderer, getPointsText(this.info), this.width / 2 + 5, startY + 15, 0xFFFFFF);
-            context.drawTextWithShadow(this.textRenderer, getRankText(this.info), this.width / 2 + 5, startY + 30, 0xFFFFFF);
-            context.drawTextWithShadow(this.textRenderer, "Rankings:", this.width / 2 + 5, startY + 45, 0xFFFFFF);
-        } else {
-            String text = this.everythingIsAwesome ? "Loading..." : "Unknown player";
-            context.drawCenteredTextWithShadow(this.textRenderer, text, this.width / 2, this.height / 2, 0xFFFFFF);
-        }
-    }
-
-    private boolean textureExists(Identifier texture) {
-        return MinecraftClient.getInstance().getTextureManager().getOrDefault(texture, null) != null;
-    }
-
-    private CompletableFuture<Identifier> fetchTexture(String user) {
-        String username = user.toLowerCase();
-        Identifier tex = Identifier.of(TierTagger.MOD_ID, "player_" + username);
-
-        if (textureExists(tex)) return CompletableFuture.completedFuture(tex);
-
-        TextureManager texManager = MinecraftClient.getInstance().getTextureManager();
-        HttpRequest req = HttpRequest.newBuilder(URI.create("https://mc-heads.net/body/" + username + "/240")).GET().build();
-
-        return HTTP_CLIENT.sendAsync(req, HttpResponse.BodyHandlers.ofByteArray()).thenApply(r -> {
-            if (r.statusCode() == 200) {
-                try {
-                    NativeImage image = NativeImage.read(r.body());
-                    texManager.registerTexture(tex, new NativeImageBackedTexture(image));
-                } catch (IOException e) {
-                    TierTagger.getLogger().error("Failed to register head texture", e);
-                }
-            } else {
-                TierTagger.getLogger().error("Could not fetch head texture: {} {}", r.statusCode(), new String(r.body()));
-            }
-
-            return tex;
-        });
+        context.drawTextWithShadow(this.textRenderer, getRegionText(this.info), this.width / 2 + 5, startY, 0xFFFFFFFF);
+        context.drawTextWithShadow(this.textRenderer, getPointsText(this.info), this.width / 2 + 5, startY + 15, 0xFFFFFFFF);
+        context.drawTextWithShadow(this.textRenderer, getRankText(this.info), this.width / 2 + 5, startY + 30, 0xFFFFFFFF);
+        context.drawTextWithShadow(this.textRenderer, "Rankings:", this.width / 2 + 5, startY + 45, 0xFFFFFFFF);
     }
 
     private Text formatTier(@NotNull GameMode gamemode, PlayerInfo.Ranking tier) {
